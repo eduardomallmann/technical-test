2fff810ef9266330890ce3c869265b8352954835
eduardomallmann <mallmann.edu@gmail.com>
2020-08-22 18:33:21 -0300

    Add util classes to build responses
    Signed-off-by: eduardomallmann <mallmann.edu@gmail.com>

---
+++ src/main/java/com/eduardomallmann/compasso/technicaltest/util/Builder.java
@@ -0,0 +1,135 @@
+package com.eduardomallmann.compasso.technicaltest.util;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Builder pattern implementation.
+ * <p>Acquired from https://howtocodetutorial.wordpress.com/generic-builder-pattern-in-java-8 in 07/22/2019</p>
+ *
+ * @param <T> The class of the object that will be sent in the response content.
+ *
+ * @author eduardomallmann
+ * @since 0.0.1
+ */
+public class Builder<T> {
+
+    private T instance;
+    private boolean ifCond = true;
+
+    /**
+     * Private constructor to be use internally only.
+     *
+     * @param clazz   the class of the instance
+     * @param objects parameters to instantiate the constructor
+     */
+    private Builder(Class<T> clazz, Object... objects) {
+        try {
+            Stream<Constructor<?>> constructors = Stream.of(clazz.getDeclaredConstructors()).peek(constructor -> constructor.setAccessible(true));
+            Optional<Constructor<?>> constructor;
+            if (null == objects) {
+                constructor = constructors.filter(cons -> cons.getParameterCount() == 0).findFirst();
+                if (constructor.isPresent()) {
+                    instance = (T) constructor.get().newInstance();
+                }
+            } else {
+                constructor = constructors.filter(cons -> cons.getParameterCount() == objects.length)
+                                      .filter(cons -> {
+                                          List<Class<?>> consClass = Arrays.asList(cons.getParameterTypes());
+                                          List<Class<?>> paramsClass = Arrays.stream(objects).map(Object::getClass).collect(Collectors.toList());
+                                          return consClass.stream()
+                                                         .allMatch(con -> paramsClass.stream()
+                                                                                  .anyMatch(param -> (con.isPrimitive() &&
+                                                                                                              (Number.class.isAssignableFrom(param) ||
+                                                                                                                       Boolean.class.isAssignableFrom(param) ||
+                                                                                                                       Character.class.isAssignableFrom(param) ||
+                                                                                                                       Byte.class.isAssignableFrom(param))) ||
+                                                                                                             param.equals(con) ||
+                                                                                                             param.isAssignableFrom(con) ||
+                                                                                                             con.isAssignableFrom(param)));
+                                      })
+                                      .findFirst();
+                if (constructor.isPresent()) {
+                    instance = (T) constructor.get().newInstance(objects);
+                }
+            }
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Instantiate a new Builder.
+     *
+     * @param clazz class of the instance
+     * @param <T>   type of the class
+     *
+     * @return an instance of builder.
+     */
+    public static <T> Builder<T> create(Class<T> clazz) {
+        return new Builder<>(clazz, (Object[]) null);
+    }
+
+    /**
+     * Instantiate a new Builder with parameters.
+     *
+     * @param clazz   class of the instance
+     * @param <T>     type of the class
+     * @param objects parameters to instantiate the constructor
+     *
+     * @return an instance of builder.
+     */
+    public static <T> Builder<T> create(Class<T> clazz, Object... objects) {
+        return new Builder<>(clazz, objects);
+    }
+
+    /**
+     * Set a value into a paramater of the instance.
+     *
+     * @param setter Consumer to insert the value
+     *
+     * @return the current builder.
+     */
+    public Builder<T> with(Consumer<T> setter) {
+        if (ifCond)
+            setter.accept(instance);
+        return this;
+    }
+
+    /**
+     * @return the instance to the producer.
+     */
+    public T build() {
+        return instance;
+    }
+
+    /**
+     * Creates a flux control to insert the value of the parameters.
+     *
+     * @param condition Condition to be analized
+     *
+     * @return the current builder.
+     */
+    public Builder<T> If(BooleanSupplier condition) {
+        this.ifCond = condition.getAsBoolean();
+        return this;
+    }
+
+    /**
+     * Finalize the flux control.
+     *
+     * @return the current builder.
+     */
+    public Builder<T> endIf() {
+        this.ifCond = true;
+        return this;
+    }
+}
+
---
+++ src/main/java/com/eduardomallmann/compasso/technicaltest/util/Response.java
@@ -0,0 +1,209 @@
+package com.eduardomallmann.compasso.technicaltest.util;
+
+import org.springframework.data.domain.Page;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Class responsible for encapsulate the body of the responseBuilder e return an standard object.
+ *
+ * @author eduardomallmann
+ * @since 0.0.1
+ */
+public class Response<T> {
+
+
+    private List<T> content;
+    private long numberOfElements;
+    private long pageSize;
+    private long pageNumber;
+    private long totalPages;
+    private long totalElements;
+    private boolean first;
+    private boolean last;
+    private boolean empty;
+
+    /**
+     * Private constructor to be used internally.
+     */
+    private Response() {
+    }
+
+    /**
+     * Instantiates the {@link ResponseBuider}.
+     *
+     * @return a new instance of {@link ResponseBuider}.
+     */
+    public static ResponseBuider builder() {
+        return new ResponseBuider(Builder.create(Response.class));
+    }
+
+    /**
+     * Creates a new instance of {@link Response} with the list informed as content. This method should be used when the responseBuilder is a list with all the data from
+     * the dataset.
+     *
+     * @param list the responseBuilder
+     * @param <T>  The class of the object that will be sent in the content.
+     *
+     * @return A new instance of {@link Response}.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Response<T> of(final List<T> list) {
+        if (list == null || list.isEmpty()) return builder().empty(true).build();
+        return builder()
+                       .content(list)
+                       .numberOfElements(list.size())
+                       .pageSize(list.size())
+                       .pageNumber(0)
+                       .totalPages(1)
+                       .totalElements(list.size())
+                       .first(true)
+                       .last(true)
+                       .empty(false)
+                       .build();
+    }
+
+    /**
+     * Creates a new instance of {@link Response} with the object informed. This method should be used when the object to be sent is not a collection.
+     *
+     * @param object the object to be sent  in the content.
+     * @param <T>    The class of the object that will be sent in the content.
+     *
+     * @return A new instance of {@link Response}.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Response<T> of(final T object) {
+        if (object == null) return builder().empty(true).build();
+        return of(Collections.singletonList(object));
+    }
+
+    /**
+     * Creates a new instance of {@link Response} with the page informed. This method should be used when the object to be sent in the responseBuilder is a {@link Page}.
+     *
+     * @param page the page with the content to be sent in the responseBuilder
+     * @param <T>  The class of the object that will be sent in the content.
+     *
+     * @return A new instance of {@link Response}.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Response<T> of(final Page<T> page) {
+        if (page == null || page.getContent().isEmpty()) return builder().empty(true).build();
+        return builder()
+                       .content(page.getContent())
+                       .numberOfElements(page.getNumberOfElements())
+                       .pageSize(page.getSize())
+                       .pageNumber(page.getNumber())
+                       .totalPages(page.getTotalPages())
+                       .totalElements(page.getTotalElements())
+                       .first(page.isFirst())
+                       .last(page.isLast())
+                       .empty(page.isEmpty())
+                       .build();
+    }
+
+    public static class ResponseBuider<T> {
+
+        public Builder<Response<T>> builder;
+
+        public ResponseBuider(final Builder<Response<T>> builder) {
+            this.builder = builder;
+        }
+
+        public ResponseBuider content(final List<T> content) {
+            this.builder.with(r -> r.content = content);
+            return this;
+        }
+
+        public ResponseBuider content(final T content) {
+            this.builder.with(r -> {
+                if (r.content == null) {
+                    r.content = new ArrayList<>();
+                }
+                r.content.add(content);
+            });
+            return this;
+        }
+
+        public ResponseBuider numberOfElements(final long numberOfElements) {
+            this.builder.with(r -> r.numberOfElements = numberOfElements);
+            return this;
+        }
+
+        public ResponseBuider pageSize(final long pageSize) {
+            this.builder.with(r -> r.pageSize = pageSize);
+            return this;
+        }
+
+        public ResponseBuider pageNumber(final long pageNumber) {
+            this.builder.with(r -> r.pageNumber = pageNumber);
+            return this;
+        }
+
+        public ResponseBuider totalPages(final long totalPages) {
+            this.builder.with(r -> r.totalPages = totalPages);
+            return this;
+        }
+
+        public ResponseBuider totalElements(final long totalElements) {
+            this.builder.with(r -> r.totalElements = totalElements);
+            return this;
+        }
+
+        public ResponseBuider first(final boolean first) {
+            this.builder.with(r -> r.first = first);
+            return this;
+        }
+
+        public ResponseBuider last(final boolean last) {
+            this.builder.with(r -> r.last = last);
+            return this;
+        }
+
+        public ResponseBuider empty(final boolean empty) {
+            this.builder.with(r -> r.empty = empty);
+            return this;
+        }
+
+        public Response<T> build() {
+            return this.builder.build();
+        }
+    }
+
+    public List<T> getContent() {
+        return content;
+    }
+
+    public long getNumberOfElements() {
+        return numberOfElements;
+    }
+
+    public long getPageSize() {
+        return pageSize;
+    }
+
+    public long getPageNumber() {
+        return pageNumber;
+    }
+
+    public long getTotalPages() {
+        return totalPages;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public boolean isFirst() {
+        return first;
+    }
+
+    public boolean isLast() {
+        return last;
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+}
